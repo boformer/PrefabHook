faed25561bdb16c7cf76aa7bdfee5d2d63d71a16
earalov <earalov@thumbtack.net>
2016-11-12 00:45:37 +0600

    Add reflective API for BuildingInfoHook to provide means of BuildingThemes compatibility

---
+++ PrefabHook/ReflectiveApi/BuildingInfoHookReflective.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrefabHook.ReflectiveApi
+{
+    public static class BuildingInfoHookReflective
+    {
+        public static void RegisterPreInitializationHook(Action<BuildingInfo> action)
+        {
+            BuildingInfoHook.OnPreInitialization += action.Invoke;
+        }
+
+        public static void RegisterPostInitializationHook(Action<BuildingInfo> action)
+        {
+            BuildingInfoHook.OnPostInitialization += action.Invoke;
+        }
+
+        public static void Deploy()
+        {
+            BuildingInfoHook.Deploy();
+        }
+
+        public static void Revert()
+        {
+            BuildingInfoHook.Revert();
+        }
+    }
+}
